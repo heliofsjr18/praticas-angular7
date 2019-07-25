fdd04bc3772afde0362fe45b147b4490d9c07013
Santos <isabella.v.santos@accenture.com>
2019-07-25 12:58:05 -0300

    metodos de validação

--- praticas-angular/src/app/isabella/isabella.component.ts
+++ praticas-angular/src/app/isabella/isabella.component.ts
@@ -2,9 +2,6 @@ import { Validators } from '@angular/forms'
 import {FormControl, FormGroup} from '@angular/forms';
 import {Component} from '@angular/core';
 
-
-
-
 @Component({
   selector: 'app-isabella',
   templateUrl: './isabella.component.html',
@@ -13,32 +10,22 @@ import {Component} from '@angular/core';
 
 export class IsabellaComponent
  {
-
-
    formDados = new FormGroup
   ({
-    nome: new FormControl('',Validators.required),
+    nome: new FormControl('',[Validators.required,Validators.minLength(4),Validators.pattern(/[A-Za-z-áéíóúÁÉÍÓÚ]/)]),
     idade: new FormControl('',Validators.required),
-    cpf: new FormControl('',[Validators.required,Validators.minLength(11),Validators.maxLength(11)]),
-    telefone: new FormControl('',Validators.required),
-    area: new FormControl('',[Validators.minLength(3),Validators.minLength(3)]),
-    
-    exchange: new FormControl('',[Validators.minLength(4),Validators.minLength(5)]),
-    subscriber: new FormControl('',[Validators.minLength(4),Validators.minLength(4)]),
+    cpf: new FormControl('',[Validators.required,Validators.minLength(11),Validators.pattern(/[0-9]/)]),
+    telefone: new FormControl('',[Validators.required,Validators.minLength(11), Validators.pattern(/[0-9]/)]),
     email: new FormControl('',[Validators.required,Validators.email]),
-    
-    Endereco: new FormGroup({
-    rua: new FormControl('',Validators.required),
-    cidade: new FormControl('',Validators.required),
-    estado: new FormControl('',Validators.required)
-  })
+    rua: new FormControl('',[Validators.required,Validators.minLength(4)]),
+    cidade: new FormControl('',[Validators.required,Validators.pattern(/[A-Za-z-áéíóúÁÉÍÓÚ]/)]),
+    estado: new FormControl('',[Validators.required,Validators.pattern(/[A-Za-z-áéíóúÁÉÍÓÚ]/)]),
   });
 
-   onSubmit() 
-  {
-    
-    console.warn(this.formDados.value);
+   onSubmit() {
+    ;
   }
+
 //Preenchimento automatico para verificar a validação
   updateProfile() {
     this.formDados.patchValue
@@ -56,17 +43,80 @@ export class IsabellaComponent
       }
     });
   }
-  getErrorMessage() {
-    return this.formDados.controls.email.hasError('required') ? 'You must enter a value' :
-        this.formDados.controls.email.hasError('email') ? 'Not a valid email' :
+
+  getErrorMessageNome() {
+    var mensagem:string = '';
+
+    if(this.formDados.controls.nome.hasError('required'))
+    {
+        mensagem = 'O campo é obrigatório';
+    }
+    else if(this.formDados.controls.nome.hasError('pattern')){
+      mensagem = 'Apenas Letras';
+    }
+    else if(this.formDados.controls.nome.hasError('minlength')){
+        mensagem = 'Minimo 4 digitos';
+    }
+    return mensagem;
+  }
+
+  getErrorMessageEmail() {
+    return this.formDados.controls.email.hasError('required') ? 'O campo é obrigatório' :
+        this.formDados.controls.email.hasError('email') ? 'O email não é válido' :
             '';
   }
 
   getErrorMessageTelefone() {
-    return this.formDados.controls.telefone.hasError('required') ? 'You must enter a value' :
-        this.formDados.controls.telefone.hasError('telefone') ? 'Not a valid number' :
-            '';
+    var mensagem:string = '';
+
+    if(this.formDados.controls.telefone.hasError('required'))
+    {
+        mensagem = 'You must enter a value';
+    }
+    else if(this.formDados.controls.telefone.hasError('pattern')){
+      mensagem = 'Apenas numeros';
+    }
+    else if(this.formDados.controls.telefone.hasError('minlength')){
+        mensagem = 'Minimo 11 digitos';
+    }
+    else if(this.formDados.controls.telefone.hasError('maxlength')){
+      mensagem = 'Max 12 digitos';
+    }
+
+    return mensagem;
+  }
+ 
+  getErrorMessageCpf() {
+    var mensagem:string = '';
+
+    if(this.formDados.controls.cpf.hasError('pattern')){
+      mensagem = 'Apenas numeros';
+    }
+    else if(this.formDados.controls.cpf.hasError('required')){
+        mensagem = 'O campo é obrigatório';
+    }
+     
+    else if(this.formDados.controls.cpf.hasError('minlength')){
+        mensagem = 'O CPF não é válido';
+      }
+
+    return mensagem;
   }
 
+  getErrorMessageRua() {
+    return this.formDados.controls.rua.hasError('required') ? 'O campo é obrigatório' :
+        this.formDados.controls.rua.hasError('minlength') ? 'Não é um endereço válido' :
+            '';
+  }
   
+  getErrorMessageCidade() {
+    return this.formDados.controls.cidade.hasError('required') ? 'O campo é obrigatório' :
+        this.formDados.controls.cidade.hasError('pattern') ? 'Apenas letras' :
+            '';
+  }
+  getErrorMessageEstado() {
+    return this.formDados.controls.estado.hasError('required') ? 'O campo é obrigatório' :
+        this.formDados.controls.estado.hasError('pattern') ? 'Apenas letras' :
+            '';
+  }
 }
